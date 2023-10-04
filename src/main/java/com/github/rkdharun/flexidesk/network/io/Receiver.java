ca18035d2b67ae4561e966ef7890c59337612e21
rkdharun <dharunrk47@gmail.com>
2023-10-05 00:11:46 +0530

    ADDED: Prompt for file saving destination

--- src/main/java/com/github/rkdharun/flexidesk/network/io/Receiver.java
+++ src/main/java/com/github/rkdharun/flexidesk/network/io/Receiver.java
@@ -1,34 +1,42 @@
 package com.github.rkdharun.flexidesk.network.io;
 
 import com.github.rkdharun.flexidesk.network.packets.FilePacket;
+import javafx.application.Platform;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 import java.io.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Receiver {
   public ObjectInputStream objectInputStream;
-  public Receiver(ObjectInputStream objectInputStream){
-    this.objectInputStream =  objectInputStream;
+
+  public Receiver(ObjectInputStream objectInputStream) {
+    this.objectInputStream = objectInputStream;
   }
 
-  public static void receiveFile(ObjectInputStream objectInputStream){
+  public static void receiveFile(ObjectInputStream objectInputStream) {
     try {
       DirectoryChooser directoryChooser = new DirectoryChooser();
       directoryChooser.setTitle("Select Directory to save file");
-      File selectedDirectory = new File("C:/Users/dharu/Desktop/");
+      final File[] selectedDirectory = {null};
+      AtomicBoolean isFinished = new AtomicBoolean(false);
+      Platform.runLater(
+        () -> {
+          Stage s = new Stage();
+          selectedDirectory[0] = directoryChooser.showDialog(s);
+          System.out.println(selectedDirectory[0].getAbsolutePath());
+          isFinished.set(true);
+        }
+      );
 
-//             Platform.runLater(
-//                    () -> {
-//                      Stage s = new Stage();
-//                      selectedDirectory = directoryChooser.showDialog(s);
-//                      System.out.println(selectedDirectory.getAbsolutePath());
-//                    }
-//            );
+      while (!isFinished.get()){
+      }
 
       System.out.println("File packet Receiving");
       FilePacket fp;
       fp = (FilePacket) objectInputStream.readObject();
-
 
 
       System.out.println("File packet received");
@@ -37,11 +45,11 @@ public class Receiver {
 
 
       byte[] fileData = new byte[1024 * 1024];
-      File tempFile = new File(selectedDirectory.getAbsolutePath() + "/" + fp.getFileName().trim());
+      File tempFile = new File(selectedDirectory[0].getAbsolutePath() + "/" + fp.getFileName().trim());
       tempFile.createNewFile();
       FileOutputStream fos = new FileOutputStream(tempFile);
 
-      Long toRead= fp.getFileLength();
+      Long toRead = fp.getFileLength();
       int read = 0;
       Long totalReceived = 0l;
       byte[] payload = new byte[1024 * 1024];
@@ -50,14 +58,14 @@ public class Receiver {
       while (totalReceived < toRead && (read = objectInputStream.read(payload)) != -1) {
         fos.write(payload, 0, read);
         totalReceived += read;
-        System.out.println(totalReceived +  "--MB---"+toRead);
+        System.out.println(totalReceived + "--MB---" + toRead);
       }
-        System.out.println("File Received");
-        fos.close();
-        System.out.println("File Received");
+      System.out.println("File Received");
+      fos.close();
+      System.out.println("File Received");
 
     } catch (ClassNotFoundException | IOException e) {
-        throw new RuntimeException(e);
+      throw new RuntimeException(e);
 
     }
 
@@ -67,7 +75,5 @@ public class Receiver {
   public static void receiveClip(ObjectInputStream objectInputStream) {
   }
 
-  public static String receiveHeader(int headerLen){
-    byte[] buff = new byte[headerLen];
-  }
+
 }
