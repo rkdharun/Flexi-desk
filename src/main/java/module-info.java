a61114fd8e5d5540b6852546590813a663b2b91d
rkdharun <dharunrk47@gmail.com>
2023-12-10 10:39:55 +0530

    Jlink Packaging

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,12 @@
-module com.github.rkdharun.flexi {
+module com.github.rkdharun.flexidesk {
   requires javafx.controls;
   requires javafx.fxml;
-  requires javafx.media;
+  requires javafx.graphics;
   requires java.logging;
-  requires java.desktop;
-
-  requires org.bouncycastle.pkix;
-  requires org.bouncycastle.provider;
-  requires com.google.zxing;
-
+//  requires org.bouncycastle.pkix;
+//  requires org.bouncycastle.provider;
+  //requires com.google.zxing;
   opens com.github.rkdharun.flexidesk to javafx.fxml;
-
-
   exports com.github.rkdharun.flexidesk;
   opens com.github.rkdharun.flexidesk.controller.ui to javafx.fxml;
 }
